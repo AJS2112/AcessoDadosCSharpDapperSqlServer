daa42a47a2b5ec2fce4ff5c3dc30f8c6c0619430
Antonio Sucre <antonio.sucre@indikatore.com>
2021-07-12 08:04:33 -0300

    CRUD

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ namespace BlogBalta
         private const string CONNECTION_STRING = "Data source=localhost; Initial Catalog=BlogBalta; Persist Security Info=True; Integrated Security=SSPI;";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ReadUsers();
+            //ReadUser();
+            //CreateUser();
+            UpdateUser();
         }
 
         public static void ReadUsers()
@@ -22,6 +25,62 @@ namespace BlogBalta
                 {
                     Console.WriteLine(user.Name);
                 }
+            }
+        }
+
+        public static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+                Console.WriteLine(user.Name);
+            }
+        }
+
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Bio = "Batman",
+                Email = "batman@gmail.com",
+                Image = "https://",
+                Name = "Batman",
+                PasswordHash = "HASH",
+                Slug = "batman"
+            };
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+                Console.WriteLine("Cadastro realizado con sucesso");
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Bio = "Batman The Hero",
+                Email = "batman@gmail.com",
+                Image = "https://",
+                Name = "Batman",
+                PasswordHash = "HASH",
+                Slug = "batman"
+            };
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+                Console.WriteLine("Atualização realizada con sucesso");
+            }
+        }
+
+        public static void DeleteUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(2);
+                connection.Delete<User>(user);
+                Console.WriteLine("Exclusão realizada con sucesso");
             }
         }
     }
